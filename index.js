9476a41f4bf50bbbaf8a45948553399d80e58ca0
ericX08 <ericX08@users.noreply.github.com>
2024-07-01 02:24:27 +0000

    deploy: bfe395e2fcaec73d94621c83272e619c373b4927

---
+++ index.js
@@ -0,0 +1,27 @@
+import css from "/css.svg"
+import arrow from "/arrow.svg"
+import javascript from "/javascript.svg"
+import blender from "/blender-original.svg"
+import csharp from "/csharp-original.svg"
+import email from "/email.svg"
+import github from "/github.svg"
+import html from "/html.svg"
+import java from "/java-original.svg"
+import leetcode from "/leetcode.svg"
+import linkedin from "/linkedin.svg"
+import php from "/php-original.svg"
+import python from "/python-original.svg"
+import resume from "/resume.svg"
+import typescript from "/typescript.svg"
+import wordpress from "/wordpress-plain.svg"
+import xamarin from "/xamarin-original.svg"
+import work from "/work.svg"
+import edu from "/edu.svg"
+import about from "/about.svg"
+import load from "/loader.gif"
+
+
+export {css, arrow, javascript, blender, csharp, email, github,
+    html, java, leetcode, linkedin, php, python, resume, typescript,
+    wordpress, xamarin, work, edu, about, load
+}
